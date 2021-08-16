a2e855930b12f82eddd6786634b48cb59545275c
ToshitaC <toshitachaudhari17@gmail.com>
2021-08-16 17:03:58 +0530

    Added: even odd file

---
+++ src/com/bridgelabs/evenodd/EvenOdd.java
@@ -0,0 +1,16 @@
+package com.bridgelabs.evenodd;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+
+		int num = 6;
+	if (num%2==0) {
+		System.out.println("The Num is even");
+	}
+	else 
+		System.out.println("The Num is odd");
+
+	}
+
+}
