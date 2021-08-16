7edf9ff166856a23e953c901496891ee2dbb4806
ToshitaC <toshitachaudhari17@gmail.com>
2021-08-16 17:13:25 +0530

    Refactored: Removed Extra Spaces

--- src/com/bridgelabs/evenodd/EvenOdd.java
+++ src/com/bridgelabs/evenodd/EvenOdd.java
@@ -5,11 +5,10 @@ public class EvenOdd {
 	public static void main(String[] args) {
 
 		int num = 6;
-	if (num%2==0) {
-		System.out.println("The Num is even");
-	}
-	else 
-		System.out.println("The Num is odd");
+		if (num % 2 == 0) {
+			System.out.println("The Num is even");
+		} else
+			System.out.println("The Num is odd");
 
 	}
 
